15ee2ed4cb610183b251e1b9cb8af8210b0bd720
a_Volunteers <silky_slasher_0w@icloud.com>
2023-03-14 13:11:55 +0900

    first commit for Atm

---
+++ atm/src/atm/AccountManager.java
@@ -0,0 +1,15 @@
+package atm;
+
+import java.util.*;
+
+public class AccountManager {
+	
+	private static ArrayList<Account> list = new ArrayList<Account>();
+	
+	// Account에 대한
+	
+	// Create
+	// Read
+	// Update
+	// Delete
+}
---
+++ atm/src/atm/UserManager.java
@@ -0,0 +1,15 @@
+package atm;
+
+import java.util.ArrayList;
+
+public class UserManager {
+
+	private static ArrayList<Account> list = new ArrayList<Account>();
+
+	// User에 대한
+	
+	// Create
+	// Read
+	// Update
+	// Delete
+}
